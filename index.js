c21c5f6908497b5f728c468003b871c301bcd2ed
D. E. Ackeret <101536271+DEAckeret@users.noreply.github.com>
2024-01-26 21:51:28 -0700

    layout nearly finished.

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ $(document).ready(function() {
     console.log("ready!");
 });
 
+
+
+$(document).ready(function() {
+    $(window).resize(function() {
+        if ($(window).width() > 574) {
+            $('#news-image').attr('src', '/assets/images/illustration-sign-up-desktop.svg');
+        } else {
+            $('#news-image').attr('src', '/assets/images/illustration-sign-up-mobile.svg');
+        }
+    }).resize(); // Trigger the resize event immediately
+});
+
